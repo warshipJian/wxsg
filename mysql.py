2b362f29b23eedbb85587bc3b9ab7e6fb4ccb48b
warshipJian <J.GY.jian@gmail.com>
2019-01-07 10:54:07 +0800

    fix

--- mysql.py
+++ mysql.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 def connect():
     # 创建数据库连接
     db_conf = config.database()
-    engine = create_engine('mysql://%s:%s@localhost:3306/%s?charset=utf8mb4' %
-                           (db_conf['user'], db_conf['password'], db_conf['database']),
+    engine = create_engine('mysql://%s:%s@%s:3306/%s?charset=utf8mb4' %
+                           (db_conf['user'], db_conf['password'],db_conf['host'],db_conf['database']),
                            pool_pre_ping=True)
     DBSession = sessionmaker(bind=engine)
     return DBSession()
